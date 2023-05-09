fb1a2ef9bd7c289bcbd1f4fab82bd521235576e2
Naa003 <nathan.anderson.1024@gmail.com>
2023-05-08 17:41:57 -0700

    updated controller functions to work

--- controller/index.js
+++ controller/index.js
@@ -1,49 +1,48 @@
 /* eslint-disable prettier/prettier */
 const formidable = require('formidable');
-const {create, get, remove } = require('../model/todo')
+const { create, get, remove } = require('../model/todo');
 
 exports.create = (req, res) => {
-    const form = new formidable.IncomingForm();
-    form.keepExtensions = true;
-    form.parse(req, async (err, fields) => {
-        const {description} = fields;
-        
-        if (!fields.description) {
-            return res.status(400).json({
-                error: 'Description is required',
-            });
-        }
-        try {
-            const newTask = await create(description);
-            return res.status(201).send({data: newTask.rows[0]})
-        } catch (error) {
-            return res.status(400).json({
-                error,
-            })
-        }
-    })
+  const form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, async (err, fields) => {
+    const { description } = fields;
+    // check for all fields
+    if (!fields.description) {
+      return res.status(400).json({
+        error: 'Description is required',
+      });
+    }
+    try {
+      const newTask = await create(description);
+      return res.status(201).send({ data: newTask.rows[0] });
+    } catch (error) {
+      return res.status(400).json({
+        error,
+      });
+    }
+  });
 };
 
 exports.read = async (req, res) => {
-    try {
-        const data = await get();
-        const json = await data.json();
-        return res.status(200).send(json);
-    } catch (err) {
-        return res.status(400).json({
-            error: err
-        });
-    }
+  try {
+    const task = await get();
+    return res.json({ data: task.rows });
+  } catch (err) {
+    return res.status(400).json({
+      error: err,
+    });
+  }
 };
 
 exports.removeTodo = async (req, res) => {
-    const id = Number(req.params.id);
-    try {
-        await remove(id)
-        return res.status(200).send({data: id})
-    } catch (error) {
-        return res.status(400).json({
-            error
-        });
-    }
+  const id = Number(req.params.id);
+  try {
+    await remove(id);
+    return res.status(200).send({ data: id });
+  } catch (error) {
+    return res.status(400).json({
+      error,
+    });
+  }
 };
